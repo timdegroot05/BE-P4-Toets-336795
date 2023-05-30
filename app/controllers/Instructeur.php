24a8fff9dcd36d32a3d7758949b76db8bb5fc878
timdegroot05 <336795@student.mboutrecht.nl>
2023-05-30 11:38:22 +0200

    commit 1

---
+++ app/controllers/Instructeur.php
@@ -0,0 +1,109 @@
+<?php
+
+class Instructeur extends BaseController
+{
+    private $instructeurModel;
+
+    public function __construct()
+    {
+        $this->instructeurModel = $this->model('InstructeurModel');
+    }
+
+    public function overzichtInstructeur()
+    {
+        $result = $this->instructeurModel->getInstructeurs();
+
+        //  var_dump($result);
+        $rows = "";
+        foreach ($result as $instructeur) {
+            /**
+             * Datum in het juiste formaat gezet
+             */
+            $date = date_create($instructeur->DatumInDienst);
+            $formatted_date = date_format($date, 'd-m-Y');
+
+            $rows .= "<tr>
+                        <td>$instructeur->Voornaam</td>
+                        <td>$instructeur->Tussenvoegsel</td>
+                        <td>$instructeur->Achternaam</td>
+                        <td>$instructeur->Mobiel</td>
+                        <td>$formatted_date</td>            
+                        <td>$instructeur->AantalSterren</td>            
+                        <td>
+                            <a href='" . URLROOT . "/instructeur/overzichtvoertuigen/$instructeur->Id'>
+                                <i class='bi bi-car-front'></i>
+                            </a>
+                        </td>            
+                      </tr>";
+        }
+        
+        $data = [
+            'title' => 'Instructeurs in dienst',
+            'rows' => $rows
+        ];
+
+        $this->view('Instructeur/overzichtinstructeur', $data);
+    }
+
+    public function overzichtVoertuigen($Id)
+    {
+
+        $instructeurInfo = $this->instructeurModel->getInstructeurById($Id);
+
+        $instructeurNaam = $instructeurInfo->Voornaam . " " . $instructeurInfo->Tussenvoegsel . " " . $instructeurInfo->Achternaam;
+
+
+        
+
+        /**
+         * We laten de model alle gegevens ophalen uit de database
+         */
+        $result = $this->instructeurModel->getToegewezenVoertuigen($Id);
+
+        $tableRows = "";
+        if (empty($result)) {
+            /**
+             * Als er geen toegewezen voertuigen zijn komt de onderstaande tekst in de tabel
+             */
+            $tableRows = "<tr>
+                            <td colspan='6'>
+                                Er zijn op dit moment nog geen voertuigen toegewezen aan deze instructeur
+                            </td>
+                          </tr>";
+        } else {
+            /**
+             * Bouw de rows op in een foreach-loop en stop deze in de variabele
+             * $tabelRows
+             */
+            foreach ($result as $voertuig) {
+
+                /**
+                 * Zet de datum in het juiste format
+                 */
+                $date_formatted = date_format(date_create($voertuig->Bouwjaar), 'd-m-Y');
+
+                $tableRows .= "<tr>
+                                    <td>$voertuig->TypeVoertuig</td>
+                                    <td>$voertuig->Type</td>
+                                    <td>$voertuig->Kenteken</td>
+                                    <td>$date_formatted</td>
+                                    <td>$voertuig->Brandstof</td>
+                                    <td>$voertuig->RijbewijsCategorie</td>            
+                            </tr>";
+            }
+        }
+        
+
+        $data = [
+            'title'     => 'Door instructeur gebruikte voertuigen',
+            'tableRows' => $tableRows,
+            'naam'      => $naam,
+            'datumInDienst' => $datumInDienst,
+            'aantalSterren' => $aantalSterren
+        ];
+
+        $this->view('Instructeur/overzichtVoertuigen', $data);
+
+
+    }
+}
---
+++ app/models/InstructeurModel.php
@@ -0,0 +1,70 @@
+<?php
+
+class InstructeurModel
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+
+    public function getInstructeurs()
+    {
+        $sql = "SELECT Id
+                      ,Voornaam
+                      ,Tussenvoegsel
+                      ,Achternaam
+                      ,Mobiel
+                      ,DatumInDienst
+                      ,AantalSterren
+                FROM  Instructeur
+                ORDER BY AantalSterren DESC";
+
+        $this->db->query($sql);
+        return $this->db->resultSet();
+    }
+
+    public function getToegewezenVoertuigen($Id)
+    {
+        $sql = "SELECT       VOER.Type
+                            ,VOER.Kenteken
+                            ,VOER.Bouwjaar
+                            ,VOER.Brandstof
+                            ,TYVO.TypeVoertuig
+                            ,TYVO.RijbewijsCategorie
+
+                FROM        Voertuig    AS  VOER
+                
+                INNER JOIN  TypeVoertuig AS TYVO
+
+                ON          TYVO.Id = VOER.TypeVoertuigId
+                
+                INNER JOIN  VoertuigInstructeur AS VOIN
+                
+                ON          VOIN.VoertuigId = VOER.Id
+                
+                WHERE       VOIN.InstructeurId = $Id
+                
+                ORDER BY    TYVO.RijbewijsCategorie DESC";
+
+        $this->db->query($sql);
+        return $this->db->resultSet();
+    }
+
+    public function getInstructeurById($Id)
+    {
+        $sql = "SELECT Id
+                      ,Voornaam
+                      ,Tussenvoegsel
+                      ,Achternaam
+                      ,Mobiel
+                      ,DatumInDienst
+                      ,AantalSterren
+                FROM  Instructeur
+                WHERE Id = $Id";
+
+        $this->db->query($sql);
+        return $this->db->single();
+    }
+}
